1cfee7aadcdeaf38cf915c7a075800456d9d89e8
ipcrm <mcadorette@puppetlabs.com>
2016-02-11 15:07:29 -0500

    -updated

--- manifests/create_exp_user.pp
+++ manifests/create_exp_user.pp
@@ -1,7 +1,7 @@
 class profile_test::create_exp_user {
   user {'test_exp_user':
     ensure   => present,
-    comment  => 'Test user with expiry',
+    comment  => 'Test user'
     password => 'T3stP@ssw0rd',
   }
 }
