87be899b216fb1ece0c8c03f0b5a36533bac2cad
ipcrm <mcadorette@puppetlabs.com>
2016-02-11 12:05:26 -0500

    u

--- manifests/local_security.pp
+++ manifests/local_security.pp
@@ -4,7 +4,7 @@ class profile_test::local_security {
   }
 
   local_security_policy {'Password must meet complexity requirements':  policy_value => '1', }
-  local_security_policy {'Minimum password length':                     policy_value => '8', }
+  local_security_policy {'Minimum password length':                     policy_value => '9', }
   local_security_policy {'Account lockout threshold':                   policy_value => '3', }
   local_security_policy {'Account lockout duration':                    policy_value => '5', }
   local_security_policy {'Reset account lockout counter after':         policy_value => '30', }
