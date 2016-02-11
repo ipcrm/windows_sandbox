1160021448b72d531fbb9d6eb86991da8e8c26f2
ipcrm <mcadorette@puppetlabs.com>
2016-02-11 13:56:40 -0500

    save

--- manifests/create_exp_user.pp
+++ manifests/create_exp_user.pp
@@ -2,7 +2,6 @@ class profile_test::create_exp_user {
   user {'test_exp_user':
     ensure   => present,
     comment  => 'Test user with expiry',
-    expiry   => '2018-01-01',
     password => 'T3stP@ssw0rd',
   }
 }
--- manifests/domain_membership.pp
+++ manifests/domain_membership.pp
@@ -1,11 +1,10 @@
 class profile_test::domain_membership {
-  host{'lab.local':
-    ip => '10.20.2.10',
+  host {'lab.local':
+    ip =>  '10.0.2.15',
   } ->
-
   class{'::domain_membership':
-    domain       => 'lab.local',
-    username     => 'vagrant',
-    password     => 'vagrant',
+    domain   => 'lab.local',
+    username => 'vagrant',
+    password => 'vagrant',
   }
 }
